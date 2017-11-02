dc8d7276db9b6752cf0594cc78c7494e4a640a75
Garik <sadovyg@gmail.com>
2017-11-01 22:32:13 -0400

    Add files via upload

---
+++ smallFibonacciNumber.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <ctype.h>
+
+//Small Fibonacci Number: takes in an argument n, and finds the nth fibonacci number
+
+int main()
+{
+  int n, first = 0, second = 1, next, c;
+
+  printf("Enter the number n to find the nth number in the Fibonacci Series\n");
+  scanf("%d", &n);
+
+  if (n < 1 || n > 42) //check to see is digit \in 1..42
+    printf("smlFibNum: error - please enter a single integer as input that is \\in 1..42\n");
+  else if (n < 3) //check to see if either n = either 1 or 2 (special case)
+    printf("%d\n", n - 1);
+  else
+    {
+      for (c=3; c <= n; c++) //calculate nth fibnumber
+      {
+	next = first + second;
+	first = second;
+	second = next;
+      }
+      printf("%d\n", next);
+    }
+  return 0;
+}
+  
